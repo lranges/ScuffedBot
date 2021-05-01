2b318bd7d272dac7ec50f72a67b432a49c6f18ac
Lovely Ranges <26846746+lranges@users.noreply.github.com>
2021-05-01 14:56:31 -0700

    chat gui updated to SFU colours.

--- chatgui.py
+++ chatgui.py
@@ -68,14 +68,14 @@ import tkinter
 from tkinter import *
 
 
-def send():
+def send(self):
     msg = EntryBox.get("1.0",'end-1c').strip()
     EntryBox.delete("0.0",END)
 
     if msg != '':
         ChatLog.config(state=NORMAL)
         ChatLog.insert(END, "You: " + msg + '\n\n')
-        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+        ChatLog.config(foreground="#A6192E", font=("Verdana", 12 ))
 
         res = chatbot_response(msg)
         ChatLog.insert(END, "Bot: " + res + '\n\n')
@@ -85,12 +85,12 @@ def send():
 
 
 base = Tk()
-base.title("Hello")
+base.title("ScuffedBot")
 base.geometry("400x500")
 base.resizable(width=FALSE, height=FALSE)
 
 #Create Chat window
-ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+ChatLog = Text(base, bd=0, bg="#ffffff", height="8", width="50", font="Arial", fg='#A6192E')
 
 ChatLog.config(state=DISABLED)
 
@@ -99,19 +99,19 @@ scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
 ChatLog['yscrollcommand'] = scrollbar.set
 
 #Create Button to send message
-SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
-                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
-                    command= send )
+#SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
+  #                  bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+    #                command= send )
 
 #Create the box to enter message
-EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
-#EntryBox.bind("<Return>", send)
+EntryBox = Text(base, bd=0, bg="#A6192E",width="29", height="5", font="Arial", fg='white')
+EntryBox.bind("<Return>", send)
 
 
 #Place all components on the screen
 scrollbar.place(x=376,y=6, height=386)
 ChatLog.place(x=6,y=6, height=386, width=370)
-EntryBox.place(x=128, y=401, height=90, width=265)
-SendButton.place(x=6, y=401, height=90)
+EntryBox.place(x=6, y=401, height=90, width=370)
+#SendButton.place(x=6, y=401, height=90)
 
 base.mainloop()
